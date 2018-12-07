39f0a0215ec273b1283a10eadfb3f7eb34e26ec8
NDXCAPPIELLO <andres_alcap@hotmail.com>
2018-12-07 12:50:20 -0300

    Add files via upload

---
+++ test/codigo.js
@@ -0,0 +1,13 @@
+var lista = document.getElementsByClassName("clase");
+
+function prepararBotones(){
+  for (var i = 0; i < lista.length; i++){
+    lista[i].addEventListener("click", mostrar(lista[i].innerHTML));
+  };
+};
+
+function mostrar(texto){
+  return function(){
+    console.log(texto);
+  };
+};
